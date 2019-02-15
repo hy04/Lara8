26e7c002f4d47e4346882f7e36064f2cf00b2ae6
He Yang <heyang@Hes-MacBook-Pro.local>
2019-02-15 21:18:58 +1300

    auth add

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,7 +51,9 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'username' => ['required', 'string', 'min:4'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
+
         ]);
     }
 
@@ -66,6 +68,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'username' => $data['username'],
             'password' => Hash::make($data['password']),
         ]);
     }
--- app/Http/Controllers/BookmarksController.php
+++ app/Http/Controllers/BookmarksController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class BookmarksController extends Controller
 {
-    //
+    public function _construct(){
+      $this->middleware('auth');
+    }
+    public function index(){
+      return view('home');
+    }
 }
